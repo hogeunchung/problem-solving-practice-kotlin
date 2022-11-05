7cd41a66b4ad6c17ba185b780998389e673b46ab
wafflejuice <wafflejuice96@gmail.com>
2022-11-06 00:40:05 +0900

    Create conversion function from IntArray to ListNodes

--- src/test/kotlin/leetcode/dataStructure/extension/IntArrayExtensionKtTest.kt
+++ src/test/kotlin/leetcode/dataStructure/extension/IntArrayExtensionKtTest.kt
@@ -1,14 +1,13 @@
 package leetcode.dataStructure.extension
 
 import leetcode.dataStructure.ListNode
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
-internal class IntArrayExtentionKtTest {
+internal class IntArrayExtensionKtTest {
 
     @Test
-    fun toListNodes() {
+    fun `Convert an IntArray to ListNodes correctly`() {
         // Arrange
         val expected = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
 
