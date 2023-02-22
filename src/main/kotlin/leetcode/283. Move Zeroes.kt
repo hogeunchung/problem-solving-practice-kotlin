04940ba67b6ac76058c4fcc68bf881f016cc39da
wafflejuice <wafflejuice96@gmail.com>
2023-02-22 23:28:16 +0900

    283. Move Zeroes

---
+++ src/main/kotlin/leetcode/283. Move Zeroes.kt
@@ -0,0 +1,22 @@
+package leetcode
+
+fun moveZeroes(nums: IntArray): Unit {
+    mergeSort(nums, 0, nums.size)
+}
+
+private fun mergeSort(nums: IntArray, l: Int, r: Int) {
+    if (l >= r) return
+
+    val mid = l / 2 + r / 2 + 1
+    mergeSort(nums, l, mid)
+    mergeSort(nums, mid, r)
+
+    var lf = l
+    var rf = mid
+
+    while (true) {
+        if (nums[lf] == 0) {
+            
+        }
+    }
+}
