7fa70eebbff7045323ae3ba4a8af87814c3ff6b9
wafflejuice <wafflejuice96@gmail.com>
2023-03-17 11:25:59 +0900

    1920. Build Array from Permutation

---
+++ src/main/kotlin/leetcode/1920. Build Array from Permutation.kt
@@ -0,0 +1,10 @@
+package leetcode
+
+fun buildArray(nums: IntArray): IntArray {
+    val result = IntArray(nums.size)
+    for (i in nums.indices) {
+        result[i] = nums[nums[i]]
+    }
+
+    return result
+}
