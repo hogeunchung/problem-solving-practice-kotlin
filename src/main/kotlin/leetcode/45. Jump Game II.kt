a54b79bccd9f344affa03a7e6418752e621318e8
Hogeun Chung <wafflejuice96@gmail.com>
2025-05-14 23:52:38 +0900

    45. Jump Game II

---
+++ src/main/kotlin/leetcode/45. Jump Game II.kt
@@ -0,0 +1,19 @@
+package leetcode
+
+import kotlin.math.min
+
+fun jump(nums: IntArray): Int {
+    val n = nums.size
+    val jumpCount = IntArray(n) { Int.MAX_VALUE }
+    jumpCount[0] = 0
+
+    for (i in 0 until n) {
+        for (j in i + 1..min(i + nums[i], n - 1)) {
+            if (jumpCount[j] > jumpCount[i] + 1) {
+                jumpCount[j] = jumpCount[i] + 1
+            }
+        }
+    }
+
+    return jumpCount[n - 1]
+}
---
+++ src/test/kotlin/leetcode/_45__Jump_Game_IITest.kt
@@ -0,0 +1,31 @@
+package leetcode
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class _45__Jump_Game_IITest {
+
+    @Test
+    fun test1() {
+        // Arrange
+        val expected = 2
+
+        // Act
+        val actual = jump(intArrayOf(2, 3, 1, 1, 4))
+
+        // Assert
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test2() {
+        // Arrange
+        val expected = 2
+
+        // Act
+        val actual = jump(intArrayOf(2, 3, 0, 1, 4))
+
+        // Assert
+        assertEquals(expected, actual)
+    }
+}
