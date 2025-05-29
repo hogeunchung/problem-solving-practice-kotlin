6ae5bf74300966148098e13dfad86a73c860fe54
Hogeun Chung <wafflejuice96@gmail.com>
2025-05-29 23:03:13 +0900

    Fix: 93. Restore IP Addresses test

--- src/test/kotlin/leetcode/_93__Restore_IP_AddressesTest.kt
+++ src/test/kotlin/leetcode/_93__Restore_IP_AddressesTest.kt
@@ -42,9 +42,9 @@ class _93__Restore_IP_AddressesTest {
     }
 
     @Test
-    fun test3() {
+    fun test4() {
         // Arrange
-        val expected = listOf("1.0.10.23", "1.0.102.3", "10.1.0.23", "10.10.2.3", "101.0.2.3")
+        val expected = emptyList<String>()
 
         // Act
         val actual = restoreIpAddresses(s = "0279245587303")
