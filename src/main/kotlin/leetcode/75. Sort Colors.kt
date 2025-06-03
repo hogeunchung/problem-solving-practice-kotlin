e2e63c3db3a6588fe96c9fc360a998f036428e40
Hogeun Chung <wafflejuice96@gmail.com>
2025-06-03 23:19:11 +0900

    75. Sort Colors: Improved with Dutch National Flag Algorithm (DNF)

--- src/main/kotlin/leetcode/75. Sort Colors.kt
+++ src/main/kotlin/leetcode/75. Sort Colors.kt
@@ -1,6 +1,6 @@
 package leetcode
 
-fun sortColors(nums: IntArray): Unit {
+fun sortColors0(nums: IntArray): Unit {
     val n = nums.size
     for (i in 0 until n) {
         for (j in i + 1 until n) {
@@ -11,4 +11,35 @@ fun sortColors(nums: IntArray): Unit {
             }
         }
     }
+}
+
+fun sortColors(nums: IntArray): Unit {
+    var low = 0
+    var mid = 0
+    var high = nums.size - 1
+
+    while (mid <= high) {
+        when (nums[mid]) {
+            0 -> {
+                swap(nums = nums, i = low, j = mid)
+                low++
+                mid++
+            }
+
+            1 -> {
+                mid++
+            }
+
+            2 -> {
+                swap(nums = nums, i = high, j = mid)
+                high--
+            }
+        }
+    }
+}
+
+private fun swap(nums: IntArray, i: Int, j: Int) {
+    val temp = nums[i]
+    nums[i] = nums[j]
+    nums[j] = temp
 }
