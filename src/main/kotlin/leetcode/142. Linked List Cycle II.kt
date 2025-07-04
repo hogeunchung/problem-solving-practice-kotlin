871383ef33c586393c1c02fbf30a388a2ae4e212
Hogeun Chung <wafflejuice96@gmail.com>
2025-07-04 22:40:34 +0900

    142. Linked List Cycle II: Improve by two pointer algorithm

--- src/main/kotlin/leetcode/142. Linked List Cycle II.kt
+++ src/main/kotlin/leetcode/142. Linked List Cycle II.kt
@@ -2,7 +2,7 @@ package leetcode
 
 import leetcode.dataStructure.ListNode
 
-fun detectCycle(head: ListNode?): ListNode? {
+fun detectCycle1(head: ListNode?): ListNode? {
     var curr = head
     var count = 0
     while (curr != null) {
@@ -21,4 +21,27 @@ fun detectCycle(head: ListNode?): ListNode? {
     }
 
     return null
+}
+
+// Two pointers
+fun detectCycle(head: ListNode?): ListNode? {
+    var slowPointer = head?.next
+    var fastPointer = head?.next?.next
+
+    while (true) {
+        if (fastPointer == null) return null
+        if (slowPointer == fastPointer) break
+
+        slowPointer = slowPointer?.next
+        fastPointer = fastPointer.next?.next
+    }
+
+    var p1 = head
+    var p2 = fastPointer
+    while (true) {
+        if (p1 == p2) return p1
+
+        p1 = p1?.next
+        p2 = p2?.next
+    }
 }
