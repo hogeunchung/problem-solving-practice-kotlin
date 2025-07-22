4712bd2eeef394e77c983904c6249d9c2e1a1381
Hogeun Chung <wafflejuice96@gmail.com>
2025-07-22 23:43:20 +0900

    977. Squares of a Sorted Array: Improved time complexity

--- src/main/kotlin/leetcode/977. Squares of a Sorted Array.kt
+++ src/main/kotlin/leetcode/977. Squares of a Sorted Array.kt
@@ -1,5 +1,22 @@
 package leetcode
 
-fun sortedSquares(nums: IntArray): IntArray {
+fun sortedSquares0(nums: IntArray): IntArray {
     return nums.map { it * it }.sorted().toIntArray()
+}
+
+fun sortedSquares(nums: IntArray): IntArray {
+    val n = nums.size
+    nums.indices.forEach { i -> nums[i] = nums[i] * nums[i] }
+
+    val result = IntArray(n)
+    var left = 0
+    var right = n - 1
+    for (i in n - 1 downTo 0) {
+        when {
+            nums[left] < nums[right] -> result[i] = nums[right--]
+            else -> result[i] = nums[left++]
+        }
+    }
+
+    return result
 }
