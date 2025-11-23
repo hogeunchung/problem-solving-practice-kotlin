ab9e546b36d4bffe5257df6e4fb4f23153bef999
Hogeun Chung <wafflejuice96@gmail.com>
2025-11-24 08:24:04 +0900

    933. Number of Recent Calls: Improved time efficiency

--- src/main/kotlin/leetcode/933. Number of Recent Calls.kt
+++ src/main/kotlin/leetcode/933. Number of Recent Calls.kt
@@ -4,23 +4,21 @@ class Solution933 {
 
     class RecentCounter() {
 
-        val requests = mutableListOf<Int>()
+        val requests = IntArray(10000)
+        var currIdx = 0
         var idxOffset = 0
 
         fun ping(t: Int): Int {
-            requests.add(t)
-            var recentRequestCount = 1
+            requests[currIdx++] = t
 
-            for (i in requests.size - 2 downTo idxOffset) {
+            for (i in currIdx - 2 downTo idxOffset) {
                 if (requests[i] < t - 3000) {
                     idxOffset = i + 1
                     break
                 }
-
-                recentRequestCount++
             }
 
-            return recentRequestCount
+            return currIdx - idxOffset
         }
     }
 }
